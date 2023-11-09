5d427c6480e0adc5154564fee8243f57f205f70f
Gutsik228 <igordudkov22@gmail.com>
2023-11-09 14:13:32 +0500

    Task6

---
+++ Tyuiu.DudkovIE.Sprint4.Task6.V25.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DudkovIE.Sprint4.Task6.V25.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DudkovIE.Sprint4.Task6.V25.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4bbd1ce5-7a68-4c3a-b909-49b8baa1fdfe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.DudkovIE.Sprint4.Task6.V25/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.DudkovIE.Sprint4.Task6.V25.Lib;
+namespace Tyuiu.DudkovIE.Sprint4.Task6.V25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+
+            Console.Title = "Спринт #4 | Выполнил: Дудков И.Е | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Класс Array                                                       *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнил: Дудков И.Е  | СМАРТб-23-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан строковый массив данных, используя класс Array подсчитайте количество элементов, длина которых больше 6.*");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Массив строк:" + " " + "Теннис", "Футбол", "Крикет", "Баскетбол", "Бейсбол", "Регби", "Хоккей");
+             string[] mas = new string[] { "Теннис", "Футбол", "Крикет", "Баскетбол", "Бейсбол", "Регби", "Хоккей" };
+            
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(mas);
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
