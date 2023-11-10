f2ccf7ce7526d7533413e1184cd3396c5d8d1505
Gutsik228 <igordudkov22@gmail.com>
2023-11-10 14:58:33 +0500

    Task7

---
+++ Tyuiu.DudkovIE.Sprint4.Task7.V19/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.DudkovIE.Sprint4.Task7.V19.Lib;
+namespace Tyuiu.DudkovIE.Sprint4.Task7.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #4 | Выполнил: Дудков И.Е | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема:  Добавление к решению итоговых проектов по спринту                *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнил: Дудков И.Е  | СМАРТб-23-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр 90817264. Преобразуйте ее в матрицу 4 на 2 и подсчитайте количество нечетных чисел в матрице.*");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            
+            int n = 4;
+            int m = 2;
+            string text = "90817264";
+            Console.WriteLine("n: " + n);
+            Console.WriteLine("m: " + m);
+            Console.WriteLine("String: " + text);
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(n,m,text);
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
